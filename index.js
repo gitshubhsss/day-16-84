8b5faa867fa09c7f5126b2953df3391849bf3661
gitshubhsss <shubhamranjane16@gmail.com>
2024-06-09 13:58:27 +0530

    .\index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+// let stud1 = {
+//   name: "shubham",
+//   marks: 89,
+//   age: 22,
+//   getMarks: function () {
+//     return this.marks;
+//   },
+// };
+// let stud2 = {
+//   name: "pavan",
+//   marks: 89,
+//   age: 22,
+//   getMarks: function () {
+//     return this.marks;
+//   },
+// };
+// let stud3 = {
+//   name: "ashish",
+//   marks: 89,
+//   age: 22,
+//   getMarks: function () {
+//     return this.marks;
+//   },
+// };
+
+// let name = "sumit";
+// console.log(typeof name);
+
+// let student = ["shubham","pavan"];
+
+let arr1=[1,2,3]
+let arr2=[1,2,3]
+
+arr1.sayHello=()=>{
+    console.log("hellow am arrr");
+}
+arr2.sayHello=()=>{
+    console.log("hellow am arrr");
+}
+
+let str1="shubham";
+let str2="pavan";
